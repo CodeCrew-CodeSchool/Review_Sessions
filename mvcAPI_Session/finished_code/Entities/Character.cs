a6de09631e0b0be3cd98980f2c346683d97514d6
ConnerKT <connerthompson121@gmail.com>
2024-07-12 14:52:40 -0500

    finished code

---
+++ mvcAPI_Session/finished_code/Entities/Character.cs
@@ -0,0 +1,11 @@
+namespace finished_code;
+
+public class Character
+{
+   public int Id { get; set; }
+   public string Name { get; set; }
+
+   public string Ability { get; set; }
+
+   public string Series { get; set; }
+}
